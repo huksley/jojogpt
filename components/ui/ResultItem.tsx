4298e07734aeb1b6a0de6f99e984934df2d59986
Ruslan Gainutdinov <ruslan@sanfrancisco.fi>
2023-09-26 17:12:39 +0300

    fix: show ddg error

--- components/ui/ResultItem.tsx
+++ components/ui/ResultItem.tsx
@@ -3,6 +3,17 @@ import { Result } from "@/components/querySearch";
 import { trackLink, trackOnClick } from "./track";
 import { addLike } from "../like/addLike";
 
+const parseUrlDomain = (url?: string) => {
+  if (!url) {
+    return undefined;
+  }
+  try {
+    return new URL(url).hostname
+  } catch (e) {
+    return undefined
+  }
+}
+
 export const ResultItem = ({ item, index }: { item: Result; index: number }) => {
   const { data: upvote, mutate: invalidate } = useSWR<{ negative: 0; positive: 0 }>(
     ["/api/like", item.id, index],
@@ -13,14 +24,14 @@ export const ResultItem = ({ item, index }: { item: Result; index: number }) => 
     <div key={item.href} className="flex flex-col gap-2">
       <hr className="mb-4" />
       <h3 className="SectionSubTitle mb-4">
-        [{index + 1}] 🌐 {new URL(item.href).hostname.replace("www.", "").replace(/blog\./, "")}: {item.title}
+        [{index + 1}] 🌐 {parseUrlDomain(item.href)?.replace("www.", "").replace(/blog\./, "")}: {item.title}
       </h3>
       <div className="mb-4">{item.body}</div>
       <div className="flex flex-row justify-between gap-4">
         <a
           className="button p-4"
           href={item.href}
-          onClick={trackLink("ResultGo", { domain: new URL(item.href).hostname })}
+          onClick={trackLink("ResultGo", { domain: parseUrlDomain(item.href) })}
           rel="noopener noreferrer"
           target="_blank"
         >
@@ -31,7 +42,7 @@ export const ResultItem = ({ item, index }: { item: Result; index: number }) => 
           <div className="flex flex-row justify-end gap-2">
             <button
               className="p-4"
-              onClick={trackOnClick("Upvote", { domain: new URL(item.href).hostname }, () => {
+              onClick={trackOnClick("Upvote", { domain: parseUrlDomain(item.href) }, () => {
                 addLike({ resultId: item.id, resultIndex: index, upvote: 1 }, () => invalidate());
               })}
             >
@@ -39,7 +50,7 @@ export const ResultItem = ({ item, index }: { item: Result; index: number }) => 
             </button>
             <button
               className="p-4"
-              onClick={trackOnClick("Downvote", { domain: new URL(item.href).hostname }, () => {
+              onClick={trackOnClick("Downvote", { domain: parseUrlDomain(item.href) }, () => {
                 addLike({ resultId: item.id, resultIndex: index, upvote: -1 }, () => invalidate());
               })}
             >
